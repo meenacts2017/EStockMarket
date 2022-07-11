992e794a69376de738f9d6ec6506c4301b5b28ac
meenacts2017 <108062433+meenacts2017@users.noreply.github.com>
2022-07-11 12:59:57 +0530

    Add files via upload
    API Gateway

---
+++ Gateway.WebApi/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Gateway.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config =>
+                    config.AddJsonFile("ocelot.json"));
+                })
+            .ConfigureLogging(log => log.AddConsole());
+    }
+}
